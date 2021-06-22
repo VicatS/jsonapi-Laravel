ef417e42b5b324bfaeacce272acf10833bf618cc
Victor Castro <victorcs2704@gmail.com>
2021-06-22 01:34:48 -0400

    Implementando la ruta para crear articulos

---
+++ tests/Feature/Articles/CreateArticleTest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Tests\Feature\Articles;
+
+use App\Models\Article;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class CreateArticleTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function test_example()
+    {
+        $this->withoutExceptionHandling();
+
+        $response = $this->postJson(route('api.v1.articles.create'), [
+            'data' => [
+                'type' => 'articles',
+                'attributes' => [
+                    'title' => 'Nuevo articulo',
+                    'slug' => 'nuevo-articulo',
+                    'content' => 'Contenido del articulo'
+                ]
+            ]
+        ]);
+
+        $response->assertCreated();
+
+        $article = Article::first();
+
+        $response->assertHeader(
+            'Location',
+            route('api.v1.articles.show', $article)
+        );
+
+        $response->assertExactJson([
+            'data' => [
+                'type' => 'articles',
+                'id' => (string)$article->getRouteKey(),
+                'attributes' => [
+                    'title' => 'Nuevo articulo',
+                    'slug' => 'nuevo-articulo',
+                    'content' => 'Contenido del articulo'
+                ],
+                'links' => [
+                    'self' => route('api.v1.articles.show', $article)
+                ]
+            ]
+        ]);
+    }
+}
